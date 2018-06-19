5002af0ff5b776bf857650d4c2d26160a38b0165
Yevgeny Efter <YevgenyEfter@gmail.com>
2018-06-19 10:33:38 +0300

    Changing the copyright.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// (c) Copyright 2015 EntIT Software LLC
+/*! (c) Copyright 2015 - 2018 Micro Focus or one of its affiliates. */
 
 module.exports.By = require("./lib/locator.js").By;
 module.exports.Utils = require("./lib/utils.js").Utils;
